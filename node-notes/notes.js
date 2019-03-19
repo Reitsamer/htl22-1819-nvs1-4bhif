68cd046bb0f9b3539ad31d84ee970520dabe1fe4
Wolf Reitsamer <wr@elmar21.com>
2019-03-19 09:51:35 +0100

    Completed addNote functions (including check for duplicates)

--- node-notes/notes.js
+++ node-notes/notes.js
@@ -9,13 +9,23 @@ var addNote = (title, body) => {
     body: body
   };
 
-try {
-  // READ FILE 'notes-data.json'
-  var notesString = fs.readFileSync('notes-data.json');
-  notes = JSON.parse(notesString);
-} catch (e) {
+  try {
+    // READ FILE 'notes-data.json'
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
 
-}
+  }
+
+  // var duplicateNotes = notes.filter((note) => {
+  //   return note.title === newNote.title;
+  // });
+
+  var duplicateNotes = notes.filter((note) => note.title === newNote.title);
+
+
+  // Note with same title exits?
+  if (duplicateNotes.length > 0) return;
 
   // ADD NEW NOTE TO ARRAY
   notes.push(newNote);
