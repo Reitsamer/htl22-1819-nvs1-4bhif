256071ed747172ddfffb421dfb8c5d27e49d4773
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-05-03 09:28:47 +0200

    Extracted geocode and added call in app.js

--- node-weather-app/src/app.js
+++ node-weather-app/src/app.js
@@ -1,7 +1,7 @@
-const request = require('request')
 
-// TODO: call forecast-api
-const forecast = require('./utils/forecast')
+const { forecast } = require('./utils/forecast')
+const { geocode } = require('./utils/geocode')
+
 forecast(-75.7088, 44.1545, (error, data) => {
     if (error) {
         console.log('Error:', error)
@@ -11,23 +11,11 @@ forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Data:', data)
 })
 
-// const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/XXXLos%20Angeles.json?access_token=pk.eyJ1IjoicmVpdCIsImEiOiJjanVkM3F5aGUwaHk1NGRxcDZrcjlzaXdkIn0.bQG57GOwvmpb_41o5Kge_Q'
+geocode('Vienna', (error, data) => {
+    if (error) {
+        console.log('Error:', error)
+        return
+    }
 
-// request( {url: geocodingUrl, json:true}, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to service.')
-//         return;
-//     }
-
-//     if (response.body.features.length === 0) {
-//         console.log('Unable to find location.')
-//         return
-//     }
-
-//     const place_name = response.body.features[0].place_name
-//     const longitude = response.body.features[0].center[0]
-//     const latitude = response.body.features[0].center[1]
-
-//     console.log(`${place_name}: ${longitude}/${latitude}`)
-// })
-
+    console.log('Data:', data)
+})
---
+++ node-weather-app/src/utils/geocode.js
@@ -0,0 +1,33 @@
+const request = require('request')
+
+const geocode = (location, callback) => {
+    const geocodingUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+            encodeURIComponent(location) +
+            '.json?access_token=pk.eyJ1IjoicmVpdCIsImEiOiJjanVkM3F5aGUwaHk1NGRxcDZrcjlzaXdkIn0.bQG57GOwvmpb_41o5Kge_Q'
+
+    request({ url: geocodingUrl, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to service.')
+            return;
+        }
+
+        if (response.body.features.length === 0) {
+            callback('Unable to find location.')
+            return
+        }
+
+        const place_name = response.body.features[0].place_name
+        const longitude = response.body.features[0].center[0]
+        const latitude = response.body.features[0].center[1]
+
+        callback(undefined, {
+            longitude,
+            latitude,
+            place_name
+        })
+    })
+}
+
+module.exports = {
+    geocode
+}
