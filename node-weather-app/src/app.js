e332759460d5d8188034e1020d9091faf190220a
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-05-03 09:37:46 +0200

    Applied callback chaining, added ability to pass location as command-line parameter

--- node-weather-app/src/app.js
+++ node-weather-app/src/app.js
@@ -1,21 +1,30 @@
-
 const { forecast } = require('./utils/forecast')
 const { geocode } = require('./utils/geocode')
 
-forecast(-75.7088, 44.1545, (error, data) => {
+const location = process.argv[2]
+
+if (!location) {
+    console.log('Please provide a location.')
+    return
+}
+
+geocode(location, (error, data) => {
     if (error) {
         console.log('Error:', error)
         return
     }
 
-    console.log('Data:', data)
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            console.log('Error:', error)
+            return
+        }
+    
+    console.log('Ort:', data.place_name)
+    console.log('Vorhersage:', forecastData.summary)
+    console.log('Nachricht:', forecastData.message)
+    })
 })
 
-geocode('Vienna', (error, data) => {
-    if (error) {
-        console.log('Error:', error)
-        return
-    }
 
-    console.log('Data:', data)
-})
+
